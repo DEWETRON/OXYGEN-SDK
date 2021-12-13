a3299ca18c5750e254346fafc8c386bb9b9c4190
Matthias Straka <matthias.straka@dewetron.com>
2021-12-13 15:56:01 +0100

    sync cmake compiler files

--- cmake/OxygenPluginFunctions.cmake
+++ cmake/OxygenPluginFunctions.cmake
@@ -62,3 +62,126 @@ macro(AddResourceFile TGT SRC_FILE OUT_PATH)
 
 endmacro()
 
+macro(AddTranslationResources TGT SRC_DIR OUT_PATH)
+    get_filename_component(ABS_SRC_DIR ${SRC_DIR} ABSOLUTE)
+    
+    file(GLOB _TS_FILES CONFIGURE_DEPENDS "${ABS_SRC_DIR}/*.ts")
+    message("Translation = ${_TS_FILES}")
+    foreach(_TS_FILE ${_TS_FILES})
+        AddResourceFile(${TGT} "${_TS_FILE}" "${OUT_PATH}")
+    endforeach()
+endmacro()
+
+function(FindTranslationFiles SRC_DIR PRIMARY_LANGUAGE PRIMARY_FILE OTHER_FILES)
+    message("FindTranslationFiles(${SRC_DIR}, ${PRIMARY_LANGUAGE})")
+    file(GLOB _TS_FILES1 CONFIGURE_DEPENDS "${SRC_DIR}/*_${PRIMARY_LANGUAGE}.ts")
+    file(GLOB _TS_FILES CONFIGURE_DEPENDS "${SRC_DIR}/*.ts")
+    list(LENGTH _TS_FILES1 _TS_FILES1_LEN)
+    if(NOT ${_TS_FILES1_LEN} EQUAL 1)
+        message(FATAL_ERROR "Primary translation file was not found")
+    endif()
+    list(GET _TS_FILES1 0 _TS_FILE1)
+    message("Primary translation = ${_TS_FILE1}")
+    list(REMOVE_ITEM _TS_FILES "${_TS_FILE1}" _TS_FILES)
+    message("Other translations = ${_TS_FILES}")
+    set(${PRIMARY_FILE} ${_TS_FILE1} PARENT_SCOPE)
+    set(${OTHER_FILES} ${_TS_FILES} PARENT_SCOPE)
+endfunction()
+
+macro(AddUpdateTranslations TGT SRC_DIR PRIMARY_LANGUAGE)
+    FindTranslationFiles(${SRC_DIR} ${PRIMARY_LANGUAGE} PRIMARY_TS OTHER_TS)
+
+	if (Qt5_LCONVERT_EXECUTABLE)
+		foreach(_TS_FILE ${OTHER_TS})
+			get_filename_component(_OUTPUT_BASENAME ${_TS_FILE} NAME)
+			string(REGEX MATCH ".*_(..)\\.ts" _LANG_CODE ${_TS_FILE})
+			set(_LANG_CODE ${CMAKE_MATCH_1})
+
+			ADD_CUSTOM_TARGET(
+				update_translation_${_LANG_CODE}
+				COMMAND ${Qt5_LCONVERT_EXECUTABLE} -i ${PRIMARY_TS} ${_TS_FILE} -source-language en -target-language ${_LANG_CODE} -no-obsolete -o ${_TS_FILE}
+				COMMAND ${Unix2Dos}
+				#SOURCES ${TRANSLATION_FILES}
+				WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${LANG_CODE}
+				)
+			set_target_properties(update_translation_${_LANG_CODE} PROPERTIES EXCLUDE_FROM_DEFAULT_BUILD 1)
+			#set_target_properties(update_translation_${LANG_CODE} PROPERTIES FOLDER "odk_examples/ex_wav_export")
+		endforeach()
+	endif()
+endmacro()
+
+macro(AddTranslationList TGT SRC_DIR PRIMARY_LANGUAGE OUT_PATH)
+    FindTranslationFiles(${SRC_DIR} ${PRIMARY_LANGUAGE} PRIMARY_TS OTHER_TS)
+    get_filename_component(ABS_OUT_PATH "${OUT_PATH}" ABSOLUTE)
+    set(OUTPUT_HEADER ${ABS_OUT_PATH}/all_translations.h)
+    set(OUTPUT_FILE ${ABS_OUT_PATH}/all_translations.cpp)
+    file(REMOVE ${OUTPUT_HEADER})
+    file(REMOVE ${OUTPUT_FILE})
+
+	#Write header
+	file(APPEND ${OUTPUT_HEADER} "#pragma once\n")
+    file(APPEND ${OUTPUT_HEADER} "namespace plugin_resources {\n")
+    file(APPEND ${OUTPUT_HEADER} "  bool addAllTranslations(odk::IfHost* host);\n")
+    file(APPEND ${OUTPUT_HEADER} "}\n")
+
+	#Write source
+	file(APPEND ${OUTPUT_FILE} "#include \"odkapi_utils.h\"\n")
+	
+	get_filename_component(_OUTPUT_BASENAME ${PRIMARY_TS} NAME)
+	file(APPEND ${OUTPUT_FILE} "#include \"${_OUTPUT_BASENAME}.h\"\n")
+
+    foreach(_TS_FILE ${OTHER_TS})
+        get_filename_component(_OUTPUT_BASENAME ${_TS_FILE} NAME)
+        file(APPEND ${OUTPUT_FILE} "#include \"${_OUTPUT_BASENAME}.h\"\n")
+    endforeach()
+
+    file(APPEND ${OUTPUT_FILE} "\n")
+
+    file(APPEND ${OUTPUT_FILE} "namespace {\n")
+	file(APPEND ${OUTPUT_FILE} "  bool addTranslation(odk::IfHost* host, const char* translation_xml) {\n")
+    file(APPEND ${OUTPUT_FILE} "    if (odk::sendSyncXMLMessage(host, odk::host_msg::ADD_TRANSLATION, 0, translation_xml, strlen(translation_xml) + 1, nullptr)) {\n")
+    file(APPEND ${OUTPUT_FILE} "      return false;\n")
+    file(APPEND ${OUTPUT_FILE} "    }\n")
+    file(APPEND ${OUTPUT_FILE} "    return true;\n")
+	file(APPEND ${OUTPUT_FILE} "  }\n")
+    file(APPEND ${OUTPUT_FILE} "}\n")
+	
+
+    file(APPEND ${OUTPUT_FILE} "namespace plugin_resources {\n")
+
+    file(APPEND ${OUTPUT_FILE} "  bool addAllTranslations(odk::IfHost* host) {\n")
+
+	get_filename_component(_VARIABLE_NAME "${PRIMARY_TS}" NAME)
+	# Replace filename spaces & extension separator for C compatibility
+	string(REGEX REPLACE "\\.| |-" "_" _VARIABLE_NAME ${_VARIABLE_NAME})
+	string(TOUPPER ${_VARIABLE_NAME} _VARIABLE_NAME)
+	file(APPEND ${OUTPUT_FILE} "    bool success = addTranslation(host, reinterpret_cast<const char*>(${_VARIABLE_NAME}_data));\n")
+
+    foreach(_TS_FILE ${OTHER_TS})
+		get_filename_component(_VARIABLE_NAME "${_TS_FILE}" NAME)
+		# Replace filename spaces & extension separator for C compatibility
+		string(REGEX REPLACE "\\.| |-" "_" _VARIABLE_NAME ${_VARIABLE_NAME})
+		string(TOUPPER ${_VARIABLE_NAME} _VARIABLE_NAME)
+		file(APPEND ${OUTPUT_FILE} "    success = addTranslation(host, reinterpret_cast<const char*>(${_VARIABLE_NAME}_data)) && success;\n")
+    endforeach()
+
+    file(APPEND ${OUTPUT_FILE} "    return success;\n")
+    file(APPEND ${OUTPUT_FILE} "  }\n")
+    file(APPEND ${OUTPUT_FILE} "}\n")
+    file(APPEND ${OUTPUT_FILE} "\n")
+
+    set_source_files_properties(
+      ${OUTPUT_HEADER}
+      PROPERTIES
+      GENERATED TRUE
+      HEADER_FILE_ONLY TRUE)
+
+    set_source_files_properties(
+      ${OUTPUT_FILE}
+      PROPERTIES
+      GENERATED TRUE)
+
+    target_sources(${TGT} PRIVATE ${OUTPUT_HEADER} ${OUTPUT_FILE})
+
+endmacro()
+
