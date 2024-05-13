ec086a9402208bb31965db0c50843a4654228083
Matthias Straka <matthias.straka@dewetron.com>
2024-05-13 14:31:08 +0200

    odk: various fixed and cleanup

--- odk/framework/src/odkfw_data_requester.cpp
+++ odk/framework/src/odkfw_data_requester.cpp
@@ -160,7 +160,14 @@ namespace framework
                     else
                     {
                         auto next_region_start = (regions.front().m_region.m_begin / m_channel->getTimeBase().m_frequency);
-                        next_position = std::min(m_end_position, next_region_start);
+                        if (next_region_start >= m_current_position)
+                        {
+                            next_position = std::min(m_end_position, next_region_start);
+                        }
+                        else
+                        {
+                            next_position = m_end_position;
+                        }
                     }
 
                 }
