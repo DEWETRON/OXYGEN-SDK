DEWETRON/OXYGEN-SDK

commit 1c2666cf1b9ee702cc42d2bc2f626f34fcf3367e

remove boost from setup script & cmake

setup.py
@@ -23,17 +23,6 @@ logger = logging.getLogger("Oxygen-SDK-Setup")
 # List of OXYGEN-SDK dependencies
 dependencies_json = '''
 {
-    "Windows" : [
-        {
-            "name" : "Boost 1.70.0",
-            "url" : "https://archives.boost.io/release/1.70.0/source/boost_1_70_0.zip",
-            "check" : {
-                "exists" : "3rdparty/boost_1_70_0"
-            },
-            "after" : {
-                "extract_to": "3rdparty"
-            }
-        },
         {
             "name" : "Qt resource compiler (rcc)",
             "url" : "https://github.com/DEWETRON/OXYGEN-SDK/releases/download/OXYGEN-SDK-6.0.0/qt_resource_compiler.zip",

cmake/CMakeFunctions.cmake
@@ -411,8 +411,6 @@ macro(SetupODKEnvironment OXYGEN_SDK_PATH)
 
   SetLinkStaticRuntime()
   SetCommonOutputDirectory()
-  SetBoostOptions()
-  find_package(Boost REQUIRED)
   find_package(SDKQt REQUIRED)
 
   AddUniqueTargetFromSubdirectory(pugixml "${OXYGEN_SDK_PATH}/3rdparty/pugixml-1.9/scripts" "3rdparty/pugixml-1.9")

commit 6174530338862fb81ef68611ec6bbfde2e50b07e

setup: fix corrupted JSON dependency
Broken in 1c2666cf1b9ee702cc42d2bc2f626f34fcf3367e

setup.py
@@ -23,6 +23,7 @@ logger = logging.getLogger("Oxygen-SDK-Setup")
 # List of OXYGEN-SDK dependencies
 dependencies_json = '''
 {
+    "Windows" : [
         {
             "name" : "Qt resource compiler (rcc)",
             "url" : "https://github.com/DEWETRON/OXYGEN-SDK/releases/download/OXYGEN-SDK-6.0.0/qt_resource_compiler.zip",